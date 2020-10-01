2d3fdb8e22853b9aacc2f5753e180c2ef55a8110
Carlos Gomez <Gomez-C2@iehp.org>
2020-10-01 09:11:08 -0700

    changed to template layout rather than C# mvc routes

--- electron-reddit/Controllers/HomeController.cs
+++ electron-reddit/Controllers/HomeController.cs
@@ -11,34 +11,9 @@ namespace electron_reddit.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            Electron.IpcMain.On("getData", (args) =>
-            {
-                var posts = new Pictureizer().GetTopPictures(new List<string>{"foodporn"},"month", 20);
-                var mainWindow = Electron.WindowManager.BrowserWindows.First();
-                Electron.IpcMain.Send(mainWindow, "sendData", posts);
-            });
-
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ electron-reddit/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using Microsoft.AspNetCore.Mvc;
+using reddit_dot_net;
+
+namespace electron_reddit.Controllers
+{
+    public class MainController : Controller
+{
+    public IActionResult Index()
+    {
+        Electron.IpcMain.On("getData", (args) =>
+        {
+            var posts = new Pictureizer().GetTopPictures(new List<string> { "aww" }, "month", 20);
+            var mainWindow = Electron.WindowManager.BrowserWindows.First();
+            Electron.IpcMain.Send(mainWindow, "sendData", posts);
+        }); 
+        
+        return View();
+    }
+}
+}
